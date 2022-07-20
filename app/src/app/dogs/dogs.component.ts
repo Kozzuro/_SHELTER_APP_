74dfa0c6502bd908d83a77eb5079f280fd4c6e2f
Kozzuro <oli_jab_pk@wp.pl>
2022-07-20 22:47:27 +0200

    added ui home page idea

--- app/src/app/dogs/dogs.component.ts
+++ app/src/app/dogs/dogs.component.ts
@@ -1,22 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { map } from 'rxjs';
-import { ApiService, DogModel } from '../services/api.service';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-dogs',
   templateUrl: './dogs.component.html',
-  styleUrls: ['./dogs.component.scss']
+  styleUrls: ['./dogs.component.scss'],
 })
 export class DogsComponent implements OnInit {
+  constructor(private service: ApiService) {}
 
-  constructor(private service:ApiService) {}
+  public dogs = [];
 
-  dogs: DogModel[];
-
-  ngOnInit(){
-    this.service.getDogs().pipe(
-      map((dogs) => dogs[0])
-    );
-    console.log(this.dogs)
+  getAll() {
+    this.service.getDogs().subscribe((results) => {
+      this.dogs = results.data;
+    });
   }
+
+  ngOnInit() {
+    this.getAll();
+  }
+  
 }
--- app/src/app/services/api.service.ts
+++ app/src/app/services/api.service.ts
@@ -1,30 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { BehaviorSubject, catchError, map, Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
 
-const TOKEN = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjNrX3puYnNQdVFsT2xNT2hrRFBGcCJ9.eyJpc3MiOiJodHRwczovL2Rldi03ZmNmMy1pOS51cy5hdXRoMC5jb20vIiwic3ViIjoiTGVhT21nM1l6c3dKNnVTSWNhOVVkVlJFWm9LQVh1M0hAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vZGV2LTdmY2YzLWk5LnVzLmF1dGgwLmNvbS9hcGkvdjIvIiwiaWF0IjoxNjU4MjUzMTQ1LCJleHAiOjE2NTgzMzk1NDUsImF6cCI6IkxlYU9tZzNZenN3SjZ1U0ljYTlVZFZSRVpvS0FYdTNIIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.hEJPbLRVsFo98Gx-TnzO4Jik121QQMoGfYutxh8lm1q_7V4wYe5F6yY4mQ08QB-_3EyDwwUwE4x5Dkgj88-q7mfU-yL3y1jWGMSkxQBtqaL06iYR4WQVSoWw2AUDzWgrow7GqERRKubLqWZ2hu6ymEXRTivv1Gnd3LDa5PEPDMdMS4152wDK_AaUbDz5cQBnJFV6FrFFjgn9pntxqQ72vXodcbUXfs50UNAgsyG20pHEI29SPXX6bP5kKiQ9z3SIFvZiayDxJOj4t3uP4jzg5BkM8cngDa_v1pec-HFJY4mZ59YmcJFe5_iZRf7JiqBoD6sHkS2qpH3PMwrRCSHr1A';
+const TOKEN =
+  'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjNrX3puYnNQdVFsT2xNT2hrRFBGcCJ9.eyJpc3MiOiJodHRwczovL2Rldi03ZmNmMy1pOS51cy5hdXRoMC5jb20vIiwic3ViIjoiTGVhT21nM1l6c3dKNnVTSWNhOVVkVlJFWm9LQVh1M0hAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vZGV2LTdmY2YzLWk5LnVzLmF1dGgwLmNvbS9hcGkvdjIvIiwiaWF0IjoxNjU4MzM5MTUyLCJleHAiOjE2NTg0MjU1NTIsImF6cCI6IkxlYU9tZzNZenN3SjZ1U0ljYTlVZFZSRVpvS0FYdTNIIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.ei7s6tp00OhqD7dfyliEyKGQdC8t8qIggV1T_UVfIBhVvHZ8tNja99sXUXdgHPv04LUncGw4ZR_pE79rMxqxYtpda9hx8lbxIfcspUTziF1okqXL5Q50A_Hgdyn1baCSJP8tMlVvkUsDL-edxzbujTtpUGh6a0BsEzfdBMRJzrXD7EtqePJcFZsCP-aPMC8bODDfRTljkFDlAKg41nzgzkPiY75gxUEqTzFS7yZjb-DhH8ybZh5j11dgGIPuiSvgCwgxo4sxshYzxCdyyXfbufKZ46-DgFqBT9CSRjL_mpFpOn1xaOkHIWB9vmeXQevPw1dZR1z-p5snrXopTYF8_Q';
 const API_URL = 'localhost:3000';
 
 const headers = new HttpHeaders({
   'Content-Type': 'application/json',
-  'Authorization': `Bearer ${TOKEN}`
+  Authorization: `Bearer ${TOKEN}`,
 });
 
-export interface DogModel{
-  name: string;
-}
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-
-  constructor(private httpClient: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   private url = `http://${API_URL}/api/v1/dogs`;
 
-  getDogs(): Observable<DogModel>{
-    return this.httpClient.get<DogModel>(this.url, { headers: headers });
+  getDogs(): Observable<any> {
+    return this.http.get(this.url, { headers: headers });
   }
-  
+
 }
